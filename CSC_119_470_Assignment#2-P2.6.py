8a61febb423fec409c630b8b1aba6ce61be5b65d
denisetra <denisetra@comcast.net>
2017-09-08 12:08:02 -0600

    Add files via upload

---
+++ CSC_119_470_Assignment#2-P2.6.py
@@ -0,0 +1,27 @@
+# Topic 3
+# Assignment#2-P2.6
+# Programmer: Denise Tranberg
+# Date:09/08/2017
+# Version:1.0
+#
+######################################################################
+# 				Program Description
+######################################################################
+# •• P2.6 Write a program that prompts the user for a measurement in meters
+#     and then converts it to miles, feet, and inches.
+#
+# Horstmann, Cay S.; Necaise, Rance D.. Python for Everyone,
+#     2nd Edition (Page 81). Wiley. Kindle Edition.
+######################################################################
+#                        Variable List and Initialization
+######################################################################
+
+
+
+######################################################################
+#                      Calculate and Print the Answer
+######################################################################
+
+
+
+
---
+++ CSC_119_470_Assignment#2-P2.7.py
@@ -0,0 +1,28 @@
+# Topic 3
+# Assignment#2-P2.7
+# Programmer: Denise Tranberg
+# Date:09/08/2017
+# Version:1.0
+#
+######################################################################
+# 				Program Description
+######################################################################
+# • P2.7 Write a program that prompts the user for a radius and then prints •
+# The area and circumference of a circle with that radius •
+# The volume and surface area of a sphere with that radius
+#
+# Horstmann, Cay S.; Necaise, Rance D.. Python for Everyone,
+#     2nd Edition (Page 81). Wiley. Kindle Edition.
+######################################################################
+#                        Variable List and Initialization
+######################################################################
+
+
+
+######################################################################
+#                      Calculate and Print the Answer
+######################################################################
+
+
+
+
